6bb087022735c83b877b8a22f5ec19fd6310215d
horizonix <git@zack.pevcot.net>
2024-08-25 14:06:59 +0100

    Update validate-key.js

--- popup/validate-key.js
+++ popup/validate-key.js
@@ -1,6 +1,76 @@
+const version = 1.3;
+
 document.addEventListener('DOMContentLoaded', (event) => {
     document.getElementById('validateButton').addEventListener('click', validateKey);
 });
+
+function httpGet(url) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", url, false);
+    xmlHttp.send(null);
+    var parser = new DOMParser();
+    var doc = parser.parseFromString(xmlHttp.response, "text/html");
+    return doc.body.innerHTML;
+}
+
+function run_main_func() {
+    console.log('fn main')
+    localStorage.setItem('hasRegistered_LUNARLOADER', true);
+
+    console.log(localStorage.getItem('hasRegistered_LUNARLOADER'));
+    document.getElementById('statusOutput').textContent = 'Success! Loading..';
+
+    setTimeout(() => {
+        fetch(chrome.runtime.getURL('popup/popup.html'))
+            .then(response => response.text())
+            .then(html => {
+                document.getElementById('content').innerHTML = html;
+                document.body.removeChild(document.getElementById('not_skibididiv1'));
+                document.body.removeChild(document.getElementById('not_skibididiv2'));
+                document.body.removeChild(document.getElementById('not_skibididiv3'));
+
+                let curv = httpGet('https://alreadycode.pevcot.net/api/lunarloaderversion');
+                
+                if (Number(curv) == version) {
+                    document.getElementById('content').removeChild(document.getElementById('update_client'));
+                } else {
+                    document.getElementById('updateButton').addEventListener('click', function() {
+                        chrome.tabs.create({
+                            url: 'https://github.com/horizonix/Lunar-Loader/releases/latest/download/extract_me.zip',
+                          });
+                    });
+                }
+
+                const scriptSelectionElement = document.getElementById('scriptSelection');
+                const injectButton = document.getElementById('injectButton');
+                const logoutButton = document.getElementById('logoutButton');
+
+                function injectScript() {
+                    if (scriptSelectionElement) {
+                        const selectedScript = scriptSelectionElement.value;
+                        chrome.runtime.sendMessage({ action: selectedScript });
+                    }
+                }
+                
+                if (injectButton) {
+                    injectButton.addEventListener('click', function() {
+                        injectScript();
+                    });
+                }
+                if (logoutButton) {
+                    logoutButton.addEventListener('click', function() {
+                        localStorage.removeItem('hasRegistered_LUNARLOADER');
+                    });
+                }
+            })
+    }, 1100);
+}
+
+console.log('aoidjgikfdghnki')
+if (localStorage.getItem('hasRegistered_LUNARLOADER')) {
+    console.log('was found')
+    run_main_func();
+}
 
 async function validateKey() {
     const keyBase64 = document.getElementById('keyInput').value.trim();
@@ -14,47 +84,12 @@ async function validateKey() {
         const key = base64ToArrayBuffer(keyBase64);
 
         if (key.byteLength === 33) {
-            document.getElementById('statusOutput').textContent = 'Success! Loading..';
-
-            setTimeout(() => {
-                fetch(chrome.runtime.getURL('popup/popup.html'))
-                    .then(response => response.text())
-                    .then(html => {
-                        document.getElementById('content').innerHTML = html;
-                        async function sleep(seconds) {
-                            return new Promise((resolve) => setTimeout(resolve, seconds * 1000));
-                        }
-                        sleep(1)
-                        document.body.removeChild(document.getElementById('not_skibididiv1'));
-                        document.body.removeChild(document.getElementById('not_skibididiv2'));
-                        document.body.removeChild(document.getElementById('not_skibididiv3'));
-
-                        const scriptSelectionElement = document.getElementById("scriptSelection");
-
-                        function injectScript() {
-                            console.log('Inject function called');
-                            if (scriptSelectionElement) {
-                                const selectedScript = scriptSelectionElement.value;
-                                console.log('Selected script:', selectedScript);
-                                chrome.runtime.sendMessage({ action: selectedScript });
-                            }
-                        }
-
-                        if (injectButton) {
-                            console.log('Inject button found');
-                            injectButton.addEventListener('click', function() {
-                                console.log('Inject button clicked');
-                                injectScript();
-                            });
-                        }
-                    })
-            }, 1100);
-
+            run_main_func();
         } else {
             document.getElementById('statusOutput').textContent = 'Invalid key!';
         }
     } catch (error) {
-        document.getElementById('statusOutput').textContent = 'An error occurred!';
+        document.getElementById('statusOutput').textContent = 'An error occurred! ' + error;
     }
 }
 
